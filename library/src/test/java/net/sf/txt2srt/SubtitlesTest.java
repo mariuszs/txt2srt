c0c9e046b6d4eb08a55f89ef659218c067b36d84
Mariusz Smykuła <mariuszs@gmail.com>
2012-04-01 02:05:16 +0200

    Test subtitles

---
+++ library/src/test/java/net/sf/txt2srt/SubtitlesTest.java
@@ -0,0 +1,32 @@
+package net.sf.txt2srt;
+
+import static org.fest.assertions.Assertions.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SubtitlesTest {
+
+	private static final String SUBTITLE_TYPE = "xxx";
+	Subtitles subtitles;
+
+	@Before
+	public void setUp() throws Exception {
+		subtitles = new Subtitles(SUBTITLE_TYPE);
+	}
+
+	@Test
+	public void shouldGetSubtitlesSize() {
+		subtitles.addSubtitle(0, "test", 1);
+		subtitles.addSubtitle(2, "test", 3);
+		subtitles.addSubtitle(4, "test", 5);
+
+		assertThat(subtitles.size()).isEqualTo(3);
+	}
+
+	@Test
+	public void shouldGetSubtitlesSourceType() {
+
+		assertThat(subtitles.getSourceType()).isEqualTo(SUBTITLE_TYPE);
+	}
+
+}
